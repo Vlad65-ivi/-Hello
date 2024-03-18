e36489b2528c2d03a2b7dc0aae20f08e65536f44
Vlad65-ivi <vladkereev347@gmail.com>
2024-03-18 19:01:23 +0300

    Добавьте файлы проекта.

---
+++ Задание Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+
+            Console.WriteLine("Привет мир!");
+        }
+    }
+}
